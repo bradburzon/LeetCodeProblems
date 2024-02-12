cde288ef3d1bb42143120ba766f8a83298c5e866
Brad Burzon <bradburzon@ymail.com>
2024-02-12 11:03:36 -0800

    update TwoSum, add DeleteNode and NumberPalindrome

--- Medium/DeleteNode.java
+++ Medium/DeleteNode.java
@@ -9,6 +9,13 @@ public class DeleteNode {
 	 *     ListNode(int x) { val = x; }
 	 * }
 	 */
+	
+	public class ListNode {
+		  int val;
+			 ListNode next;
+			 ListNode(int x) { val = x; }
+	}
+
 	class Solution {
 	    public void deleteNode(ListNode node) {
 	        node. val = node.next.val;
