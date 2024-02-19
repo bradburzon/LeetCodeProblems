e420482f90b014a97d6f4cf02bd186be40f58797
Brad Burzon <bradburzon@ymail.com>
2024-02-19 15:30:52 -0800

    Optimize LongestUniqueSubstring

--- Medium/LongestUniqueSubstring.java
+++ Medium/LongestUniqueSubstring.java
@@ -1,28 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestUniqueSubstring {
 
 	class Solution {
-	    public int lengthOfLongestSubstring(String s) {
-	        String longest = "";
-	        for (int i = 0; i < s.length(); i++) {
-	            String substring = "";
-	            for (int j = i; j < s.length(); j++) {
-	                if (s.length() == 1) {
-	                    if (!substring.contains(s.charAt(j) + "")) {
-	                        substring += s.charAt(j);
-	                    }
-	                } else if (!substring.contains(s.charAt(j) + "")) {
-	                    substring += s.charAt(j);
-	                } else {
-
-	                    break;
-	                }
-	            }
-	            longest = longest.length() < substring.length() ? substring : longest;
-	            substring = "";
-	        }
-	        return longest.length();
-	    }
+		// Optimizing with dynamic sliding window
+		public int lengthOfLongestSubstring(String s) {
+			int left = 0;
+			int longest = 0;  //Window
+			Set<Character> seen = new HashSet<Character>();
+			for (int right = 0; right < s.length(); right++) {
+				char letter = s.charAt(right);
+				while (seen.contains(letter)) {
+					seen.remove(s.charAt(left));
+					left++;
+				}
+				seen.add(letter);
+				longest = Math.max(longest, right - left + 1);
+			}
+			return longest;
+		}
 	}
-
 }
