f8e1025746beac73fbc48dcf81896dcbae58bc6a
Brad Burzon <bradburzon@ymail.com>
2024-02-19 14:14:15 -0800

    Finish testing LongestUniqueSubstring

--- Medium/LongestUniqueSubstring.java
+++ Medium/LongestUniqueSubstring.java
@@ -1,24 +1,28 @@
 
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        String longest = "";
-        for (int i = 0; i < s.length(); i++) {
-            String substring = "";
-            for (int j = i; j < s.length(); j++) {
-                if (s.length() == 1) {
-                    if (!substring.contains(s.charAt(j) + "")) {
-                        substring += s.charAt(j);
-                    }
-                } else if (!substring.contains(s.charAt(j) + "")) {
-                    substring += s.charAt(j);
-                } else {
+public class LongestUniqueSubstring {
 
-                    break;
-                }
-            }
-            longest = longest.length() < substring.length() ? substring : longest;
-            substring = "";
-        }
-        return longest.length();
-    }
+	class Solution {
+	    public int lengthOfLongestSubstring(String s) {
+	        String longest = "";
+	        for (int i = 0; i < s.length(); i++) {
+	            String substring = "";
+	            for (int j = i; j < s.length(); j++) {
+	                if (s.length() == 1) {
+	                    if (!substring.contains(s.charAt(j) + "")) {
+	                        substring += s.charAt(j);
+	                    }
+	                } else if (!substring.contains(s.charAt(j) + "")) {
+	                    substring += s.charAt(j);
+	                } else {
+
+	                    break;
+	                }
+	            }
+	            longest = longest.length() < substring.length() ? substring : longest;
+	            substring = "";
+	        }
+	        return longest.length();
+	    }
+	}
+
 }
--- test/LongestUniqueSubstringTest.java
+++ test/LongestUniqueSubstringTest.java
@@ -3,53 +3,34 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class LongestUniqueSubstringTest {
+	
+	private LongestUniqueSubstring longestSubstring = new LongestUniqueSubstring();
+	private LongestUniqueSubstring.Solution solution = longestSubstring.new Solution();
+
 	@Test
-	void lengthOfLongestSubstring_emptyString_ShouldReturnOne() {
-		LongestUniqueSubstring test = new LongestUniqueSubstring();
+	void givenEmptyStringWhenLengthOfLongestSubstringIsCalledThenReturnZero() {
 		String input = "";
 
-		int outcome = test.lengthOfLongestSubstring(input);
+		int outcome = solution.lengthOfLongestSubstring(input);
 
 		assertEquals(0, outcome);
 	}
 
 	@Test
-	void lengthOfLongestSubstring_consecutiveUniqueLetters_ShouldReturnSize() {
-		LongestUniqueSubstring test = new LongestUniqueSubstring();
-		String input = "abcdefg";
-		int size = input.length();
-		
-		int outcome = test.lengthOfLongestSubstring(input);
-		
-		assertEquals(size, outcome);
-	}
-	
-	@Test
-	void lengthOfLongestSubstring_alternatingUniqueLetters_ShouldReturnTwo() {
-		LongestUniqueSubstring test = new LongestUniqueSubstring();
-		String input = "abababab";
-		
-		int outcome = test.lengthOfLongestSubstring(input);
-		
-		assertEquals(2, outcome);
-	}
-	
-	@Test
-	void lengthOfLongestSubstring_threeAlternatingUniqueLetters_ShouldReturnTwo() {
-		LongestUniqueSubstring test = new LongestUniqueSubstring();
-		String input = "abcabcabcabc";
-		
-		int outcome = test.lengthOfLongestSubstring(input);
-		
-		assertEquals(3, outcome);
-	}
+	void givenConsecutiveUniqueLettersWhenLongestSubstringIsCalledThenReturnLength() {
+		String input = "abcdfghijklmnop";
 
+		int outcome = solution.lengthOfLongestSubstring(input);
+
+		assertEquals(input.length(), outcome);
+	}
+	
 	@Test
-	void lengthOfLongestSubstring_repeatingLetter_ShouldReturnOne() {
-		LongestUniqueSubstring test = new LongestUniqueSubstring();
-		String input = "bbbbbb";
-		int outcome = test.lengthOfLongestSubstring(input);
+	void givenConcesutiveLettersWhenLongestStringIsCalledThenReturnOne() {
+		String input = "aaaaaaaaaaaaaaaa";
+
+		int outcome = solution.lengthOfLongestSubstring(input);
+
 		assertEquals(1, outcome);
 	}
-
 }
