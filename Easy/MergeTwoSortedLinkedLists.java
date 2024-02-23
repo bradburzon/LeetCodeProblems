187f8cd3ae2c67717c85eb725e8bc604d982078a
Brad Burzon <bradburzon@ymail.com>
2024-02-22 21:47:04 -0800

    Add solution for [Easy]MergeTwoSortedArrays
    https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ Easy/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,41 @@
+
+public class MergeTwoSortedLinkedLists {
+	/**
+	 * Definition for singly-linked list. public class ListNode { int val; ListNode
+	 * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+	 * ListNode next) { this.val = val; this.next = next; } }
+	 */
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	class Solution {
+		public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+			ListNode ans = new ListNode();
+			if (list1 == null) {
+				return list2;
+			} else if (list2 == null) {
+				return list1;
+			}
+			if (list1.val <= list2.val) {
+				return new ListNode(list1.val, mergeTwoLists(list1.next, list2));
+			} else {
+				return new ListNode(list2.val, mergeTwoLists(list1, list2.next));
+			}
+		}
+	}
+
+}
