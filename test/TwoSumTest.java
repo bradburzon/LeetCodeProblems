4d9b1d91d45c8f1ac339b5568dac5d85ad9101aa
Brad Burzon <bradburzon@ymail.com>
2024-02-26 13:58:42 -0800

    Update test names, remove duplicate test

--- test/TwoSumTest.java
+++ test/TwoSumTest.java
@@ -7,32 +7,22 @@ class TwoSumTest {
 	TwoSum twoSum = new TwoSum();
 
 	@Test
-	void testTwoSumExample1() {
-		int[] nums = { 2, 7, 11, 15 };
+	void givenNoSolutionWhenTwoSumIsCalledThenReturnArrayWithZeroAndLenghtMinusOneAsElement() {
+		int[] nums = { 1, 2, 3, 4 };
 		int target = 9;
 		int[] actual = twoSum.twoSum(nums, target);
 
-		int[] expected = { 0, 1 };
+		int[] expected = { 0, nums.length - 1 };
 		assertArrayEquals(expected, actual);
 	}
 
 	@Test
-	void testTwoSumExample2() {
+	void givenSolutionWhenTwoSumIsCalledThenReturnArrayWithRightIndeces() {
 		int[] nums = { 3, 2, 4 };
 		int target = 6;
 		int[] actual = twoSum.twoSum(nums, target);
 
 		int[] expected = { 1, 2 };
-		assertArrayEquals(expected, actual);
-	}
-
-	@Test
-	void testTwoSumExample3() {
-		int[] nums = { 3, 3 };
-		int target = 6;
-		int[] actual = twoSum.twoSum(nums, target);
-
-		int[] expected = { 0, 1 };
 		assertArrayEquals(expected, actual);
 	}
 }
