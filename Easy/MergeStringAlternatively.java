484c92c2104aba916cc46c3449ac4861821e8fd1
Brad Burzon <bradburzon@ymail.com>
2024-02-27 11:33:35 -0800

    Refactor MergeStringAlternatively to use Stringbuilder

--- Easy/MergeStringAlternatively.java
+++ Easy/MergeStringAlternatively.java
@@ -2,17 +2,28 @@
 public class MergeStringAlternatively {
 
 	public String mergeAlternately(String word1, String word2) {
-		int word1Length = word1.length(), word2Length = word2.length();
-		int maxLength = Math.max(word1Length, word2Length);
-		String mergedString = "";
-		for (int i = 0; i < maxLength; i++) {
-			if (i < word1Length) {
-				mergedString += word1.charAt(i);
-			}
-			if (i < word2Length) {
-				mergedString += word2.charAt(i);
+		StringBuilder stringBuilder = new StringBuilder();
+		int i = 0;
+		int j = 0;
+		int word1Length = word1.length();
+		int word2Length = word2.length();
+
+		while (i < word1Length || j < word2Length) {
+			if (i == word1Length) {
+				stringBuilder.append(word2.substring(j));
+				return stringBuilder.toString();
+			} else {
+				stringBuilder.append(word1.charAt(i));
+				i++;
+				if (j == word2Length) {
+					stringBuilder.append(word1.substring(i));
+					return stringBuilder.toString();
+				} else {
+					stringBuilder.append(word2.charAt(j));
+					j++;
+				}
 			}
 		}
-		return mergedString;
+		return stringBuilder.toString();
 	}
 }
