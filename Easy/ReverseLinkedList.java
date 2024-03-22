d7d69aaa6ffc32474fe87420d639250ec7639e3c
Brad Burzon <bradburzon@ymail.com>
2024-03-21 18:05:52 -0700

    Add solution for [Easy]ReverseLinkedList
    https://leetcode.com/problems/reverse-linked-list/description/

---
+++ Easy/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+public class ReverseLinkedList {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        for(ListNode ans = null;; head = head.next){
+            if(head  == null){
+                return ans;
+            }
+            ans = new ListNode(head.val, ans);
+        }
+    }
+}
