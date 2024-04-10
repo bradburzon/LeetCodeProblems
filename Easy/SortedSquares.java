7f7e59f76ac46da41d78e85981f56ac5929b82fe
Brad Burzon <bradburzon@ymail.com>
2024-04-10 11:44:28 -0700

    Refactor SortedSquares

--- Easy/SortedSquares.java
+++ Easy/SortedSquares.java
@@ -1,7 +1,11 @@
 import java.util.Arrays;
 
 public class SortedSquares {
-    public int[] sortedSquares(int[] nums){
-        return Arrays.stream(nums).map(number -> number * number).sorted().toArray();
+    public int[] sortedSquares(int[] nums) {
+         for(int i = 0; i < nums.length; i++){
+             nums[i] = nums[i] * nums[i];
+         }
+         Arrays.sort(nums);
+         return nums;
     }
 }
