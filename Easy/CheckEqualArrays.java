80b21e65f3018691b9ee947203c690cb35c42cd4
Brad Burzon <bradburzon@ymail.com>
2024-04-24 11:51:40 -0700

    Optimize [Easy]CheckEqualArrays

--- Easy/CheckEqualArrays.java
+++ Easy/CheckEqualArrays.java
@@ -1,10 +1,18 @@
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class CheckEqualArrays {
 
     public boolean canBeEqual(int[] arr1, int[] arr2){
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        return (Arrays.equals(arr1, arr2));
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        for (int num : arr1) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        for (int num : arr2) {
+            if (!countMap.containsKey(num) || countMap.get(num) == 0) {
+                return false;
+            }
+            countMap.put(num, countMap.get(num) - 1);
+        }
+        return true;
     }
 }
